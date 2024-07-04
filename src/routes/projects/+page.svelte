45cb38c1e6f84cc1f2d09748277b9d9572c52f45
bobbythree <mynameisbobbylewis@hotmail.com>
2024-07-03 20:19:15 -0500

    code clean-up, Font color changes

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,28 +1,31 @@
 <script>
-  import Heading from '$lib/components/heading.svelte';    
-    import { EventDispatcher } from 'three';
+  import Heading from '$lib/components/heading.svelte'; 
 
   const projects = [
     {
       title: 'Ixonia!', 
       description: 'a text based adventure game',
-      href: 'https://github.com/bobbythree/ixonia'
+      hrefCode: 'https://github.com/bobbythree/ixonia',
+      hrefPlay: '/'
     },
     {
       title: 'Bubbles', 
       description: 'a simple animation built with HTML Canvas',
-      href: 'https://github.com/bobbythree/HTML-canvas-bubbles'
+      hrefCode: 'https://github.com/bobbythree/HTML-canvas-bubbles',
+      hrefPlay: '/'
     },
     {
       title: 'Drum Machine', 
       description: 'a drum machine and sequencer built with Javascript',
-      href: 'https://github.com/bobbythree/drum-machine'
+      hrefCode: 'https://github.com/bobbythree/drum-machine',
+      hrefPlay: '/'
     },
     
   ];
   
   const icons = [
     '<svg xmlns="http://www.w3.org/2000/svg" height="33px" viewBox="0 -960 960 960" width="33px" fill="#808080"><path d="M320-240 80-480l240-240 57 57-184 184 183 183-56 56Zm320 0-57-57 184-184-183-183 56-56 240 240-240 240Z"/></svg>',
+    
     '<svg xmlns="http://www.w3.org/2000/svg" height="33px" viewBox="0 -960 960 960" width="33px" fill="#808080"><path d="M320-200v-560l440 280-440 280Zm80-280Zm0 134 210-134-210-134v268Z"/></svg>'
 
   ]
@@ -43,70 +46,18 @@
         <h2 class="title">{project.title}</h2>
         <h3 class="description">{project.description}</h3>
         <div class="icons">
-          {#each icons as icon}
-            <a href="{project.href}" target="_blank" rel="noopener noreferrer">
-              {@html icon}
-            </a>
-          {/each}        
+          <a href="{project.hrefCode}" target="_blank" rel="noopener noreferrer">
+            {@html icons[0]}
+          </a>
+          <a href="{project.hrefPlay}" target="_blank" rel="noopener noreferrer">
+            {@html icons[1]}
+          </a>
+               
         </div>        
       </div>      
     {/each}
   </div>
 </div>
-
-<!-- <div id="container">
-  <div id="project-list">
-      <div class="project">
-        <div id="ixonia">    
-          <h2 class="title">Ixonia!</h2>
-          <h3 class="description">
-            a text based adventure game      
-          </h3>
-          <div class="icons">
-            <a href="https://github.com/bobbythree/ixonia" target="_blank" rel="noopener noreferrer">
-              <svg xmlns="http://www.w3.org/2000/svg" height="33px" viewBox="0 -960 960 960" width="33px" fill="#808080"><path d="M320-240 80-480l240-240 57 57-184 184 183 183-56 56Zm320 0-57-57 184-184-183-183 56-56 240 240-240 240Z"/></svg>
-            </a>
-            <a href="/">
-              <svg xmlns="http://www.w3.org/2000/svg" height="33px" viewBox="0 -960 960 960" width="33px" fill="#808080"><path d="M320-200v-560l440 280-440 280Zm80-280Zm0 134 210-134-210-134v268Z"/></svg>
-            </a>      
-          </div>
-        </div>          
-      </div>  
-      
-      <div id="canvas">
-        <div class="project">
-          <h2 class="title">Bubbles</h2>
-          <h3 class="description">a simple animation built with HTML Canvas</h3>
-          <div class="icons">
-          <a href="https://github.com/bobbythree/HTML-canvas-bubbles" target="_blank" rel="noopener noreferrer">
-            <svg xmlns="http://www.w3.org/2000/svg" height="33px" viewBox="0 -960 960 960" width="33px" fill="#808080"><path d="M320-240 80-480l240-240 57 57-184 184 183 183-56 56Zm320 0-57-57 184-184-183-183 56-56 240 240-240 240Z"/></svg>
-          </a>
-          <a href="/">
-            <svg xmlns="http://www.w3.org/2000/svg" height="33px" viewBox="0 -960 960 960" width="33px" fill="#808080"><path d="M320-200v-560l440 280-440 280Zm80-280Zm0 134 210-134-210-134v268Z"/></svg>
-          </a>        
-        </div> 
-      </div>
-    </div>
-        
-      <div id="drum-machine">
-        <div class="project">
-          <h2 class="title">Drum Machine</h2>
-          <h3 class="description">a drum machine and sequencer built with Javascript</h3>
-          <div class="icons">
-          <a href="https://github.com/bobbythree/drum-machine" target="_blank"        rel="noopener noreferrer">
-            <svg xmlns="http://www.w3.org/2000/svg" height="33px" viewBox="0 -960 960 960" width="33px" fill="#808080"><path d="M320-240 80-480l240-240 57 57-184 184 183 183-56 56Zm320 0-57-57 184-184-183-183 56-56 240 240-240 240Z"/></svg>
-          </a>
-          <a href="/">
-            <svg xmlns="http://www.w3.org/2000/svg" height="33px" viewBox="0 -960 960 960" width="33px" fill="#808080"><path d="M320-200v-560l440 280-440 280Zm80-280Zm0 134 210-134-210-134v268Z"/></svg>
-          </a>
-        </div> 
-      </div>
-    </div>
-        
-  </div>
-</div>   -->
-
-
 
 <style>
   #heading-div {
@@ -136,14 +87,15 @@
   }
 
   .title {    
-    color: rgb(250, 192, 117)  ;
+    color: hsl(207, 33%, 60%);;
     padding: 1rem 2rem;
     padding-bottom: 0;     
   }
  
   .description {    
     color: rgb(223, 223, 223);
-    padding-left: 2rem;    
+    padding-left: 2rem;
+    font-weight: 100;   
   }
 
   .icons {    
@@ -151,9 +103,5 @@
     padding-bottom: 0.7rem;            
   }
 
-  svg:hover {
-    fill: white;
-    transition: 0.5s;
-  }
   
 </style>
