203d7e1f68aaf892929e1ea6aa46bc5f5ed92217
bobbythree <mynameisbobbylewis@hotmail.com>
2024-11-13 11:00:06 -0600

    add logos, refactor links

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,20 +1,34 @@
 <script>  
   import Heading from '$lib/components/heading.svelte'
+
+  const logos = [
+    '<i class="fa-solid fa-envelope fa-2x"></i>',
+    '<i class="fa-brands fa-github fa-2x"></i>',
+    '<i class="fa-brands fa-linkedin fa-2x"></i>'
+  ]
   
   const contacts = [
     {
       name: "email",
-      href: "mailto:mynameisbobbylewis@hotmail.com",
+      logo: logos[0],
+      linkHeading:"Shoot me an email",
+      href: "mailto:mynameisbobbylewis@hotmail.com"
     },
     {
       name: "GitHub",
-      href: "https://github.com/bobbythree",
+      linkHeading: "See all my projects here",
+      logo: logos[1],
+      href: "https://github.com/bobbythree"
     },
     {
       name: "LinkedIn",
-      href: "https://www.linkedin.com/in/bobbythree/",
+      linkHeading: "Connect with me on LinkedIn",
+      logo: logos[2],
+      href: "https://www.linkedin.com/in/bobbythree/"
     }
   ]  
+
+  
 
 </script>
 
@@ -27,9 +41,11 @@
 <div id="container">
   <div id="contact-links">
     {#each contacts as contact}
+      <h2 id="link">{contact.linkHeading}              
       <a href={contact.href} target="_blank" rel="noopener noreferrer">
-        <h2>{contact.name}</h2>
+        <div id="logo">{@html contact.logo}</div>
       </a>
+      </h2>
     {/each}    
   </div>  
 </div>
@@ -38,35 +54,43 @@
 <style>
   #heading-div {
     padding: 0 15vw;
+    display: flex;
   }
 
   #contact-links {
     display: flex;
     margin: 10rem 5rem;
-    padding: 10rem 10vw;    
+    padding: 8rem 10vw;    
     gap: 3rem;
     justify-content: space-around;
+    text-align: center;
     /* background-color: rgba(255, 255, 255, 0.08); */
     
   }
 
-  #contact-links a {       
-    color: rgb(189, 110, 149);
-    text-decoration: none; 
+  #link {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-between;
+    gap: 3rem;
+    color: hsl(207, 33%, 60%)
   }
 
-  #contact-links a:hover {
-    color: rgb(199, 167, 183);
-    text-decoration: underline;    
-    text-decoration-thickness: 2px;
+  #logo {
+    color: hsl(0, 0%, 40%);
+  }
+
+  #logo:hover {
+    color: hsl(123, 33%, 65%);
     transition: 0.5s;
   }
 
   @media screen and (max-width: 666px) {
     #contact-links {
       flex-direction: column;
-      text-align: center;
       padding: 0;
+      gap: 7rem;
     }
   }
 
